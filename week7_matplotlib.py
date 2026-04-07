50a55b67c348c5b9c5ba2d70c42c3c882333aae2
Raida Tasnim Islam <raida.t.islam@gmail.com>
2026-04-07 00:54:09 -0400

    Add Week 7 Charts 1 and 2 - survival by class and gender

--- week7_matplotlib.py
+++ week7_matplotlib.py
@@ -39,7 +39,7 @@ plt.bar(
 # --- Add three required levels ---
 plt.title ("Titanic Survival Rate by Passenger Class", fontsize=14)
 plt.xlabel("Passenger Class(1=First, 2=Second, 3=Third)", fontsize=12)
-plt.ylabel("Survival Rate (0 = 0% , 1=100%)", fontsize=12)
+plt.ylabel("Survival Rate (0=0% , 1=100%)", fontsize=12)
 
 # ---Add value labels on top of each bar
 for i, value in enumerate(survival_by_class.values):
@@ -58,3 +58,47 @@ plt.savefig("chart1_survival_by_class.png",
 plt.show()
 print("Chart 1 saved.")
 print()
+
+# ==========================================
+# Chart 2 - Survival Rate by Gender
+# Question - Did Gender dertermine survival?
+# ==========================================
+
+# --- calculate survival rate by gender ---
+survival_by_gender = df.groupby("Sex") ["Survived"].mean()
+
+# --- Open new canvas ---
+plt.figure(figsize=(8,5))
+
+# --- Draw the bars ---
+plt.bar(
+    survival_by_gender.index,
+    survival_by_gender.values,
+    color=["red", "blue"],
+    edgecolor="black",
+    width=0.4
+)
+
+#--- Add the three required levels ---
+plt.title("Titanic Survival Rate by Gender", fontsize=14)
+plt.xlabel("Gender", fontsize=12)
+plt.ylabel("Survival Rate(0=0% , 1=100%)", fontsize=12)
+
+# --- Add percentage lavels on three bars ---
+for i, value in enumerate(survival_by_gender.values):
+    plt.text(i, value+0.01, f"{float(value):.1%}",
+             ha="center" , fontsize=11)
+    
+# --- Add a references line at 50%
+plt.axhline (y=0.5, color="green",linestyle="--",
+             label="50% survival line")
+plt.legend()
+
+# --- Save and Show ---
+plt.savefig("chart2_survival_by_gender.png",
+            bbox_inches="tight", dpi=150)
+
+plt.show()
+print("chart 2 saved.")
+print()
+
